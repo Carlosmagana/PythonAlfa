6fe86140181efc1093be9dfe9f99128c331b28a0
Carlosmagana <a01701466@itesm.mx>
2017-11-12 13:32:19 -0600

    hice algo :v P.D:(taquito, 2 porfavor jovenazo)

---
+++ taquito.py
@@ -0,0 +1,7 @@
+#Programa que calcula la suma y el promadio de tres numeros
+a=int(input("Ingresa el numero 1: "))
+b=int(input("Ingresa el numero 2: "))
+c=int(input("Ingresa el numero 3: "))
+total=a+b+c
+promedio=total/3
+print("el total es "+str(total)+" y el promedio es "+str(promedio))
